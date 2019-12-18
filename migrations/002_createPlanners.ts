a88be8129a17ddaca92ed0de86257c7f67f7e4d3
oklex <alexkim.347@gmail.com>
2019-12-18 15:54:25 -0800

    planner table migration bug fix

--- migrations/002_createPlanners.ts
+++ migrations/002_createPlanners.ts
@@ -1,7 +1,7 @@
 exports.up = async knex => {
     return knex.schema.createTable("planners", table => {
       table
-        .primary(['degree', 'major']).notNull();
+        .primary(['degree', 'major']);
       table.string("degree").notNull();
       table.string('major').notNull();
     });
