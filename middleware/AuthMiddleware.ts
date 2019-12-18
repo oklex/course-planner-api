e19337a81f0734f510a2e2df370d114b10baf0eb
oklex <alexkim.347@gmail.com>
2019-12-18 15:17:52 -0800

    added error messages to all auth cases

--- middleware/AuthMiddleware.ts
+++ middleware/AuthMiddleware.ts
@@ -3,15 +3,18 @@ import * as dotenv from "dotenv";
 
 dotenv.config()
 
-let checkToken = (req, res, next) => {
-  let token = req.headers["authorization"];
-  const secretKey = process.env.SECRET_KEY;
 
-  if (token) {
-    if (token.startsWith("Bearer ")) token = token.slice(7, token.length);
-    jwt.verify(token, secretKey, (err, decoded) => {
+export const AuthMiddleware = {
+  checkToken: async (req, res, next) => {
+    let token = req.headers["authorization"];
+    const secretKey = process.env.SECRET_KEY;
+
+    if (token) {
+      if (token.startsWith("Bearer ")) { token = token.slice(7, token.length); }
+      jwt.verify(token, secretKey, (err, decoded) => {
         if (err) {
-          return res.json({
+          console.log(token, err)
+          return res.status(400).json({
             success: false,
             message: "Token is not valid"
           });
@@ -20,12 +23,32 @@ let checkToken = (req, res, next) => {
           next();
         }
       });
-  } else {
-    return res.json({
-      success: false,
-      message: "Auth token is not supplied"
-    });
-  } 
-};
+    } else {
+      return res.status(400).json({
+        success: false,
+        message: "Auth token is not supplied"
+      });
+    }
+  },
 
-export default checkToken;
+  checkSignUpInfo: async (req, res, next) => {
+    let email:string = req.body.email
+    let password:string = req.body.password
+    if (!email.includes('@') || email.length < 5) {
+      return res.json({
+        success: false,
+        message: "email is invalid; please input a correct email"
+      })
+    } else if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: "password is invalid; please input a valid password"
+      })
+    } else {
+      next()
+    }
+  }
+
+}
+
+export default AuthMiddleware;
--- routers/AuthService.ts
+++ routers/AuthService.ts
@@ -1,70 +1,74 @@
 import { Router } from "express";
 import * as jwt from "jsonwebtoken";
 import * as dotenv from "dotenv";
-import checkToken from "../middleware/AuthMiddleware";
+import AuthMiddleware from "../middleware/AuthMiddleware";
 import db from "../database/knex";
-import hashPassword from "../utils/hashPassword";
 
 dotenv.config();
 const router = Router();
 
-router.post("/login", (req, res) => {
-  try {
-    const secretKey = process.env.SECRET_KEY;
-    var email: string = req.body.email;
-    var password: string = req.body.password;
-    const mockEmail: string = "email";
-    const mockPassword: string = "password";
-    // if the login params fail, then return 401 Unauthorized
-    if (email === mockEmail && password === mockPassword) {
-      const token = jwt.sign({ email, password }, secretKey);
-      console.log("token is: ", token);
-      res.json({
-        token: token
-      });
-    } else {
-      res.status(401).send("invalid authentication");
-    }
-  } catch (err) {
-    res.status(400).send("bad request", err);
-  }
-});
-
-router.post("/signup", async (req, res) => {
-  try {
-    const userEmail: string = req.body.email;
-    const password: string = req.body.password;
-    const newsletter: boolean = req.body.newsletter === "true";
-    var hashedPassword = await hashPassword(password, userEmail)
-    // if email and password are incomplete, then return an error code 419 MISSING ARGUMENTS
-    db.insert({
-      email: userEmail,
-      password: hashedPassword,
-      newsletter: newsletter,
-      is_advisor: false
+router.post("/login", async (req, res) => {
+  const secretKey = process.env.SECRET_KEY;
+  var email: string = req.body.email;
+  var password: string = req.body.password;
+  var truePassword: string = ''
+  await db('users')
+    .where({ email: email })
+    .then(rows => {
+      truePassword = rows[0].password;
+      console.log(truePassword)
     })
-      .into("users")
-      .then(() => res.end());
-
-    // only for testing
-    // db.select()
-    // .from("users")
-    // .then(rows => {
-    //   res.send(rows);
-    // })
-  } catch (err) {
-    res.status(400).send("bad request", err);
+    .catch((e) => {
+      return res.status(400).send("email not registered; user must create an account")
+    });
+  if (password === truePassword) {
+    const token = jwt.sign({ email, password }, secretKey);
+    console.log("token is: ", token);
+    return res.json({
+      token: token
+    });
+  } else {
+    return res.status(401).send("invalid authentication");
   }
 });
 
-router.get("/check", checkToken, (req, res) => {
+router.post("/signup", AuthMiddleware.checkSignUpInfo, async (req, res) => {
+  const userEmail: string = req.body.email;
+  const password: string = req.body.password;
+  const newsletter: boolean = req.body.newsletter === "true"
+  db.insert({
+    email: userEmail,
+    password: password,
+    newsletter: newsletter,
+    is_advisor: false
+  })
+    .into("users")
+    .then(() => {
+      console.log('sign-up complete');
+      return res.send("sign-up complete")
+    })
+    .catch((e) => {
+      console.log(e.sqlMessage)
+      return res.status(400).send(e.sqlMessage);
+    })
+});
+
+router.get("/check", AuthMiddleware.checkToken, (req, res) => {
   try {
     const decoded = req.decoded;
-    const email: string = decoded.email;
-    res.json([email, decoded]);
+    res.json(decoded);
   } catch (err) {
     res.status(400).send("bad request", err);
   }
 });
+
+//dev only
+router.patch('/delete', (req, res) => {
+  db('users').del().catch((e) => {
+    console.log('deleting all users')
+    return res.status(500).send(e.sqlMessage)
+  })
+  return res.status(200).send("all test users deleted")
+})
 
 export default router;
